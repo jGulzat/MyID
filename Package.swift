2289642fdfbf9879583ed9ca2da98956a17128fe
gzheenbekkyzy <gzheenbekkyzy@nurtelecom.kg>
2025-11-18 18:35:51 +0600

    Add MyIdSDK xcframework + wrapper target

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "MyIdSDK",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "MyIdSDK",
+            targets: ["MyIdSDKWrapper"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MyIdSDKBinary",
+            path: "MyIdSDK.xcframework"
+        ),
+        .target(
+            name: "MyIdSDKWrapper",
+            dependencies: [
+                .target(name: "MyIdSDKBinary")
+            ],
+            path: "Sources/MyIdSDK",
+            linkerSettings: [
+                .linkedLibrary("c++"),
+                .unsafeFlags(["-ObjC"])
+            ]
+        )
+    ]
+)
+
